d97668e8395c999817b7d351af7372dbb10f9caf
Mortuza Hossain <mortuzahossain1997@gmail.com>
2019-01-02 22:34:19 +0600

    Update UX

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -5,6 +5,8 @@ import 'package:kimatic/util/apiinfo.dart';
 import 'dart:convert';
 import 'select_city.dart';
 
+//Image URL
+//http://openweathermap.org/img/w/04d.png
 
 class Home extends StatefulWidget {
   @override
@@ -13,7 +15,7 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
 
-  var temp,pressure,humidity,maxtemp,mintemp;
+  var temp,pressure,humidity,maxtemp,mintemp,description,icon;
   String city = "Dhaka";
 
   void showStuff() async{
@@ -24,6 +26,9 @@ class _HomeState extends State<Home> {
       pressure = data["main"]["pressure"].toString();
       maxtemp = data["main"]["temp_max"].toString();
       mintemp = data["main"]["temp_min"].toString();
+      description = data['weather'][0]["description"].toString();
+      icon = data['weather'][0]["icon"].toString();
+//      print(data['weather'][0]['description']);
     });
   }
 
@@ -31,9 +36,7 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    if(temp == null){
-      showStuff();
-    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Kimatic"),
@@ -60,6 +63,7 @@ class _HomeState extends State<Home> {
           Center(
             child: new Image.asset(
               "images/umbrella.png",
+              colorBlendMode: BlendMode.colorDodge,
               width: MediaQuery.of(context).size.width,
               height: MediaQuery.of(context).size.height,
               fit: BoxFit.fill,),
@@ -73,40 +77,48 @@ class _HomeState extends State<Home> {
               style: cityStyle(),
             ),
           ),
-          
+
+          new Container(
+//            margin: EdgeInsets.fromLTRB(0.0, MediaQuery.of(context).size.height/4, 0, 0),
+            alignment: Alignment.center,
+            margin: EdgeInsets.fromLTRB(0, 85, 0, 0),
+            child: Text( description !=null ?"$description":"",style: cityStyle(),),
+          ),
+
           Container(
             alignment: Alignment.center,
-            child: Image.asset("images/light_rain.png"),
+            child: icon==null?Text("Please Hit Refress !!",style: cityStyle(),):Image.network("http://openweathermap.org/img/w/$icon.png",scale: 0.3,),
           ),
+
 
           new Container(
             margin: EdgeInsets.fromLTRB(0.0, MediaQuery.of(context).size.height/4, 0, 0),
             alignment: Alignment.center,
-            child:Text("$temp°C",style: tempTextStyle(),),
+            child:Text(temp!=null?"$temp°C":"",style: tempTextStyle(),),
           ),
 
           new Container(
             margin: EdgeInsets.fromLTRB(10, 0, 0, 90),
             alignment: Alignment.bottomLeft,
-            child: Text("Max: $maxtemp°C",style: otherTextStyle(),),
+            child: Text(maxtemp != null?"Max: $maxtemp°C":"",style: otherTextStyle(),),
           ),
 
           new Container(
             margin: EdgeInsets.fromLTRB(10, 0, 0, 65),
             alignment: Alignment.bottomLeft,
-            child: Text("Min : $mintemp°C",style: otherTextStyle(),),
+            child: Text(maxtemp != null?"Min : $mintemp°C":"",style: otherTextStyle(),),
           ),
 
           new Container(
             margin: EdgeInsets.fromLTRB(10, 0, 0, 37),
             alignment: Alignment.bottomLeft,
-            child: Text("Presure: $pressure",style: otherTextStyle(),),
+            child: Text(maxtemp != null?"Presure: $pressure":"",style: otherTextStyle(),),
           ),
 
           new Container(
             margin: EdgeInsets.fromLTRB(10, 0, 0, 10),
             alignment: Alignment.bottomLeft,
-            child: Text("Humidity: $humidity",style: otherTextStyle(),),
+            child: Text(maxtemp != null?"Humidity: $humidity":"",style: otherTextStyle(),),
           )
 
         ],
