82b9506251eac15dfd7b3a43bf9dac19a554df59
Mortuza Hossain <mortuzahossain1997@gmail.com>
2019-01-02 20:18:54 +0600

    Load Data From API Done

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:http/http.dart' as http;
+import 'package:kimatic/util/apiinfo.dart';
+import 'dart:convert';
 
 
 class Home extends StatefulWidget {
@@ -7,6 +11,23 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  var temp,pressure,humidity,maxtemp,mintemp;
+  String city = "London";
+
+  void showStuff() async{
+    Map data = await getWeather(city);
+    setState(() {
+      temp = data["main"]["temp"].toString();
+      humidity  = data["main"]["humidity"].toString();
+      pressure = data["main"]["pressure"].toString();
+      maxtemp = data["main"]["temp_max"].toString();
+      mintemp = data["main"]["temp_min"].toString();
+    });
+  }
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,8 +36,14 @@ class _HomeState extends State<Home> {
         backgroundColor: Colors.redAccent,
         centerTitle: true,
         actions: <Widget>[
-          IconButton(icon: Icon(Icons.menu), onPressed: ()=> debugPrint("Menu Press"))
+          IconButton(icon: Icon(Icons.menu), onPressed: showStuff )
         ],
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: showStuff,
+        child: Icon(Icons.refresh),
+        backgroundColor: Colors.redAccent,
       ),
       
       body: new Stack(
@@ -33,7 +60,7 @@ class _HomeState extends State<Home> {
             alignment: Alignment.topRight,
             margin: EdgeInsets.fromLTRB(0.0, 10.0, 10.0, 10.0),
             child: Text(
-              "DHAKA",
+              "${city.toUpperCase()}",
               style: cityStyle(),
             ),
           ),
@@ -46,7 +73,31 @@ class _HomeState extends State<Home> {
           new Container(
             margin: EdgeInsets.fromLTRB(0.0, MediaQuery.of(context).size.height/4, 0, 0),
             alignment: Alignment.center,
-            child: Text("67.8 °F",style: tempTextStyle(),),
+            child: Text("$temp°C",style: tempTextStyle(),),
+          ),
+
+          new Container(
+            margin: EdgeInsets.fromLTRB(10, 0, 0, 90),
+            alignment: Alignment.bottomLeft,
+            child: Text("Max: $maxtemp°C",style: otherTextStyle(),),
+          ),
+
+          new Container(
+            margin: EdgeInsets.fromLTRB(10, 0, 0, 65),
+            alignment: Alignment.bottomLeft,
+            child: Text("Min : $mintemp°C",style: otherTextStyle(),),
+          ),
+
+          new Container(
+            margin: EdgeInsets.fromLTRB(10, 0, 0, 37),
+            alignment: Alignment.bottomLeft,
+            child: Text("Presure: $pressure",style: otherTextStyle(),),
+          ),
+
+          new Container(
+            margin: EdgeInsets.fromLTRB(10, 0, 0, 10),
+            alignment: Alignment.bottomLeft,
+            child: Text("Humidity: $humidity",style: otherTextStyle(),),
           )
 
         ],
@@ -54,6 +105,36 @@ class _HomeState extends State<Home> {
       
     );
   }
+
+  Future<Map> getWeather(String city) async {
+//    String url = "http://api.openweathermap.org/data/2.5/weather?q=$city&appid=${appID}&units=matric";
+    String url = "http://api.openweathermap.org/data/2.5/weather?q=$city&appid=$appID&units=metric";
+    http.Response response = await http.get(url);
+    return json.decode(response.body);
+  }
+
+
+//  Widget updateTempWidget(String city){
+//    return FutureBuilder(
+//      future: getWeather(city),
+//      builder: (BuildContext context,AsyncSnapshot<Map> snapshot){
+//        if (snapshot.hasData){
+//          Map content = snapshot.data;
+//          return new Container(
+//            child: new Column(
+//              children: <Widget>[
+//                new ListTile(
+//                  title: Text(content['main']["temp"].toString() + " °F" ,style: tempTextStyle(),),
+//                )
+//              ],
+//            ),
+//          );
+//        }
+//      },
+//    );
+//
+//  }
+
 }
 
 
@@ -72,5 +153,15 @@ TextStyle tempTextStyle(){
     fontStyle: FontStyle.normal,
     fontWeight: FontWeight.w500,
     fontSize: 49.9
+  );
+}
+
+
+TextStyle otherTextStyle(){
+  return TextStyle(
+      color: Colors.white,
+      fontStyle: FontStyle.normal,
+      fontWeight: FontWeight.w400,
+      fontSize: 30.0
   );
 }
